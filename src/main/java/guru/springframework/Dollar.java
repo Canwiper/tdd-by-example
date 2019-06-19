2a74771266cdd90affe239cea64a132a30f3a049
Canwiper <tim@kannewischer.de>
2019-06-19 17:13:34 +0200

    New class money

--- src/main/java/guru/springframework/Dollar.java
+++ src/main/java/guru/springframework/Dollar.java
@@ -1,8 +1,7 @@
 package guru.springframework;
 
-public class Dollar {
+public class Dollar extends Money {
 
-    int amount;
 
     public Dollar(int amount) {
         this.amount = amount;
@@ -11,4 +10,5 @@ public class Dollar {
     Dollar times(int multiplier) {
         return new Dollar(amount * multiplier);
     }
+
 }
